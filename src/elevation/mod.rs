7541fc295b2710de76daacf4cb3bc5c52d191066
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-10 18:06:27 +0300

    Update mod.rs

--- src/elevation/mod.rs
+++ src/elevation/mod.rs
@@ -1,9 +1,19 @@
 use std::{env, fs};
+use log::{debug, info};
 
 #[derive(Debug)]
 pub enum ElevationError
 {
     UnknownError
+}
+
+#[derive(Debug, PartialEq)]
+enum Quarter
+{
+    TopLeft = 0,
+    TopRight = 1,
+    BottomLeft = 2,
+    BottomRight = 3
 }
 
 pub fn init()
@@ -13,12 +23,14 @@ pub fn init()
 
 pub fn scan_relative_directory(relative_directory: &str) -> Result<(), ElevationError>
 {
+    info!("Scanning relative directory: {}", &relative_directory);
     let abs_path = format!(
-        "{}/{}", env::current_dir()
+        "{}{}{}", env::current_dir()
             .unwrap()
             .into_os_string()
             .into_string()
             .unwrap(),
+        std::path::MAIN_SEPARATOR,
         relative_directory
     );
 
@@ -27,5 +39,24 @@ pub fn scan_relative_directory(relative_directory: &str) -> Result<(), Elevation
 
 pub fn scan_directory(directory: &String) -> Result<(), ElevationError>
 {
+    info!("Scanning absolute directory: {}", &directory);
+    let paths = fs::read_dir(directory).unwrap();
+    for path in paths
+    {
+        let quarter_path = path
+            .as_ref()
+            .unwrap()
+            .path()
+            .into_os_string()
+            .into_string()
+            .unwrap();
+        let quarter_name = path
+            .as_ref()
+            .unwrap()
+            .file_name()
+            .into_string()
+            .unwrap();
+        debug!("Name: {}, Path: {}", quarter_name, quarter_path);
+    }
     Ok(())
 }
