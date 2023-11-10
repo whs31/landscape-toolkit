1a119753d17f481e3570714d71ec49f0bc55be66
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-10 17:11:29 +0300

    Clean

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use crate::tiles::Preload;
-use crate::tiles::Preload::PreloadTile;
 
-mod tiles;
 
 fn main() {
-    let a = tiles::elevation(60.032398, 30.230101, PreloadTile);
-    match a
-    {
-        Some(x) => println!("{}", x),
-        None => println!("Error!")
-    };
+    println!("Hello world!");
 }
