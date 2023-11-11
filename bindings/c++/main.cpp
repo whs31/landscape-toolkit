2d00daa3ecc2ba23be947481ed087d93afa2a450
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-11 22:52:19 +0300

    Some fixes

--- bindings/c++/main.cpp
+++ bindings/c++/main.cpp
@@ -6,7 +6,7 @@ int main()
   LandscapeToolkit::Internal::enableLogger();
   std::cout << std::boolalpha
             << LandscapeToolkit::loadDirectory("elevations", LandscapeToolkit::LoadMode::Relative) << std::endl;
-  auto a = LandscapeToolkit::elevationAt(60, 30);
+  auto a = LandscapeToolkit::elevationAt(60.1, 30.1);
   if(a.has_value())
     std::cout << a.value() << std::endl;
   else
