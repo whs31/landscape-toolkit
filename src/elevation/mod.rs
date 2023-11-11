3df0d0d958fbfd057c4419e6d7fbf12119334870
whs31 <75864292+whs31@users.noreply.github.com>
2023-11-11 18:40:50 +0300

    C FFI

--- src/elevation/mod.rs
+++ src/elevation/mod.rs
@@ -23,11 +23,6 @@ pub fn init_logger()
     pretty_env_logger::init();
 }
 
-pub fn elevation_at(coordinate: (f64, f64)) -> Result<f32, ElevationError>
-{
-    elevation_impl::elevation_at(coordinate)
-}
-
 pub fn scan_relative_directory(relative_directory: &str) -> Result<(), ElevationError>
 {
     scan::scan_relative_directory(relative_directory)
@@ -36,5 +31,10 @@ pub fn scan_relative_directory(relative_directory: &str) -> Result<(), Elevation
 pub fn scan_directory(directory: &String) -> Result<(), ElevationError>
 {
     scan::scan_directory(directory)
+}
+
+pub fn elevation_at(coordinate: (f64, f64)) -> Result<f32, ElevationError>
+{
+    elevation_impl::elevation_at(coordinate)
 }
 
--- src/ffi_exports.rs
+++ src/ffi_exports.rs
@@ -1,1 +1,60 @@
+use std::ffi::{c_char, CStr};
+use crate::elevation;
 
+#[repr(C)]
+pub struct LEDResult
+{
+    pub result: f32,
+    pub valid: bool
+}
+
+#[no_mangle]
+#[allow(dead_code)]
+pub extern "C" fn led_init_logger()
+{
+    elevation::init_logger();
+}
+
+#[no_mangle]
+#[allow(dead_code)]
+pub fn led_load_relative_directory(path: *const c_char) -> bool
+{
+    match elevation::scan_relative_directory(c_char_to_string(path).as_str()) {
+        Ok(_) => true,
+        Err(_) => false
+    }
+}
+
+#[no_mangle]
+#[allow(dead_code)]
+pub fn led_load_directory(path: *const c_char) -> bool
+{
+    match elevation::scan_directory(&c_char_to_string(path)) {
+        Ok(_) => true,
+        Err(_) => false
+    }
+}
+
+#[no_mangle]
+#[allow(dead_code)]
+pub fn led_elevation_at(latitude: f64, longitude: f64) -> LEDResult
+{
+    match elevation::elevation_at((latitude, longitude)) {
+        Ok(x) => LEDResult {
+            result: x,
+            valid: true
+        },
+        Err(_) => LEDResult {
+            result: 0.0,
+            valid: false
+        }
+    }
+}
+
+fn c_char_to_string(ptr: *const c_char) -> String
+{
+    unsafe { CStr::from_ptr(ptr) }
+        .to_str()
+        .map(|s| s.to_owned())
+        .unwrap()
+}
